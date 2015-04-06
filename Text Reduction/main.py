926c3c2c709b624a0192d6259bba6650be788545
musicakc <amitoj1606@gmail.com>
2015-04-06 23:18:22 +0530

    Writing functions for retrieving text

--- Text Reduction/main.py
+++ Text Reduction/main.py
@@ -29,3 +29,29 @@ def reduce(self,text,reductionRatio):
     for s,r in reducedSentences:
         reducedText.append(s.getFullSentence())
     return reducedText
+
+def getWords(self,sentenceText, stopWords):
+    return self.tag(self.tokenize(sentenceText), stopWords)
+
+def getSentences(self,line,stopWords):
+    sentences = []
+    sentenceTexts = filter(lambda w: w.strip())
+    sentenceEnds = re.findall('[{0}]'.format(self.sentenceEndCharacters),line)
+    sentenceEnds.reverse()
+    for t in sentenceTexts:
+        if len(sentenceEnds) > 0:
+            t+=sentenceEnds.pop()
+        sentence = Sentence()
+        sentence.Words = self.getWords(t, stopWords)
+        sentences.append(sentence)
+    return sentences
+
+def getParagraphs(self,lines,stopWords):
+    paragraphs=[]
+    for line in lines:
+        paragraph=Paragraph()
+        paragraph.Sentences=self.getSentences(line, stopWords)
+        paragraphs.append(paragraph)
+    return paragraphs
+
+ 
