8e750ea3368858f8b85313d395c0eba4849f5cee
musicakc <amitoj1606@gmail.com>
2015-04-07 13:56:28 +0530

    Adding sWords text file and finishing classes

--- Text Reduction/main.py
+++ Text Reduction/main.py
@@ -20,13 +20,13 @@ class Reduction:
                 return True
         return False
 
-    def isFunction(self,text,stopWords):
-        for w in stopWords:
+    def isFunction(self,text,sWords):
+        for w in sWords:
             if text.lower()==w.lower():
                 return True
         return False
 
-    def tag(self,sampleWords,stopWords):
+    def tag(self,sampleWords,sWords):
         taggedWords = []
         for w in sampleWords:
             tw = Word()
@@ -35,19 +35,20 @@ class Reduction:
                 tw.Type = WordType.ContentPunctuation
             elif self.isFunctionPunctuation(w):
                 tw.Type = WordType.FunctionPunctuation
-            elif self.isFunction(w, stopWords):
+            elif self.isFunction(w, sWords):
                 tw.Type = WordType.Function
             else:
                 tw.Type = WordType.Content
             taggedWords.append(tw)
         return taggedWords
 
+    #Tokenizes sentences by splitting words
     def tokenize(self,text):
         return filter(lambda w: w!='',re.split('[{0}]'.format(self.punctuationCharacters),text))
     
     def reduce(self,text,reductionRatio):
-        stopWordsFile='stopWords.txt'
-        stopWords=open(stopWordsFile).read().splitlines()
+        sWordsFile='sWords.txt'
+        sWords=open(sWordsFile).read().splitlines()
 
         lines=text.splitlines()
         # Removing trailing whitespaces
@@ -77,10 +78,10 @@ class Reduction:
             reducedText.append(s.getFullSentence())
         return reducedText
 
-    def getWords(self,sentenceText, stopWords):
-        return self.tag(self.tokenize(sentenceText), stopWords)
+    def getWords(self,sentenceText, sWords):
+        return self.tag(self.tokenize(sentenceText), sWords)
 
-    def getSentences(self,line,stopWords):
+    def getSentences(self,line,sWords):
         sentences = []
         sentenceTexts = filter(lambda w: w.strip())
         sentenceEnds = re.findall('[{0}]'.format(self.sentenceEndCharacters),line)
@@ -89,14 +90,85 @@ class Reduction:
             if len(sentenceEnds) > 0:
                 t+=sentenceEnds.pop()
             sentence = Sentence()
-            sentence.Words = self.getWords(t, stopWords)
+            sentence.Words = self.getWords(t, sWords)
             sentences.append(sentence)
         return sentences
 
-    def getParagraphs(self,lines,stopWords):
+    def getParagraphs(self,lines,sWords):
         paragraphs=[]
         for line in lines:
             paragraph=Paragraph()
-            paragraph.Sentences=self.getSentences(line, stopWords)
+            paragraph.Sentences=self.getSentences(line, sWords)
             paragraphs.append(paragraph)
         return paragraphs
+
+
+class Graph:
+    def __init__(self):
+        self.Vertices = []
+        self.edges = []
+
+    def getRankedVertices(self):
+        res = defaultdict(float)
+        for e in self.Edges:
+            res[e.Vertex1]+=e.Weight
+        return sorted(res.items(), key=lambda x: x[1], reverse=True)
+
+
+class Vertex:
+    def __init__(self):
+        self.Sentence = None
+
+
+class Edge:
+    def __init__(self):
+        self.Vertex1 = None
+        self.Vertex2 = None
+        self.Weight = None
+
+
+class WordType:
+    Content = 0
+    Function = 1
+    ContentPunctuation = 2
+    FunctionPunctuation = 3
+    
+
+class Word:
+    def __init__(self):
+        self.Text=''
+        self.Type=''
+
+class Sentence:
+    def __init__(self):
+        self.Words=[]
+
+    def getFullSentence(self):
+        text = ''
+        for w in self.Words:
+            text +=w.Text
+        return text.strip()
+
+    def getReducedSentence(self):
+        sentenceText = ''
+        sentenceEnd = self.Words[len(self.Words)-1]
+        contentWords = filter(lambda w:w.Type == WordType.Content, self.Words)
+        i=0
+        while i < len(contentWords):
+            w=contentWords[i]
+            if i==0:
+                li= list(w.Text)
+                li[0] = li[0].upper()
+                w.Text = ''.join(li)
+            sentenceText += w.Text
+            if i <len(contentWords)-1:
+                sentenceText += ' '
+            elif sentenceEnd.Text != w.Text:
+                sentenceText += sentenceEnd
+            i = i+1
+        return sentenceText
+
+
+class Paragraph:
+    def __init__(self):
+        self.sentences = []
